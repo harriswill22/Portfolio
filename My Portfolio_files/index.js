732278bd8a9d8fbd0f5126189d69d8f7d37dfaf1
Will Harris <Harriswill22@yahoo.com>
2018-12-07 09:42:16 -0500

    adds resume

--- My Portfolio_files/index.js
+++ My Portfolio_files/index.js
@@ -1,4 +1,4 @@
-document.ready(function () {
+$(document).ready(function () {
     var image = $('.img-profile');
     var aboutPosition = $('.about-myself').offset().top / 1.5;
 
@@ -33,4 +33,24 @@ document.ready(function () {
         });
     });
 
+});
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > 49) {
+            $('body').addClass('header-fixed');
+        } else {
+            $('body').removeClass('header-fixed');
+        }
+        // change the style of the navbar when the user scrolls into the next zone.
+        // get the distance of the 2nd section from the top of the page - height of header.
+        var topOffset = $('#demosection2').offset().top;
+        var headerHeight = $('#topnav').height();
+        var transitionPoint = topOffset - headerHeight;
+        if (scrollTop > transitionPoint) {
+            $('#topnav').addClass('alt-header');
+        } else {
+            $('#topnav').removeClass('alt-header');
+        }
+    });
 });
